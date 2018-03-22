dd3aac4cfd759f74e97cb14858db2792eae2f5ed
mikerspencer <spencer.mike.r@gmail.com>
2018-03-22 22:10:31 +0000

    updated delegate list

--- code/anonymise.R
+++ code/anonymise.R
@@ -6,11 +6,11 @@
 
 library(tidyverse)
 
-df = read_csv("http://bit.ly/SRUC-delegates")
+df = read_csv("~/Downloads/Network analysis external delegates (Responses) - Form responses 1.csv")
 
 df$`Your name` = paste("Person", 1:nrow(df))
 
 df = df %>% 
-   select(-`Email address`)
+   select(-`Do you have any extra access or dietary requirements?`)
 
 write_csv(df, "~/repo/network-intro/data/SNA_anon_delegates.csv")
